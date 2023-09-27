06981b0300b476f2e84ea4ccc5720e53c5a58da2
HorrorOTB <nicatabdullazade135@gmail.com>
2023-09-28 00:03:07 +0400

    remove prefix

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,6 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  //Base Routing
-  app.setGlobalPrefix('/v1/api');
-
   const PORT = process.env.PORT || 9000;
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
