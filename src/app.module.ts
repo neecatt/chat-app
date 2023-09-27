ec718a02815466def483760e6b6da68cd6014e57
HorrorOTB <nicatabdullazade135@gmail.com>
2023-09-27 19:03:08 +0400

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ChatModule],
   controllers: [],
   providers: [],
 })
